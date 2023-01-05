edd7215d9661a2f27bb64488cf577248fab06947
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-01-05 16:54:40 -0300

    ultimando detalles

---
+++ scroll.js
@@ -0,0 +1,20 @@
+function scrolltoId1() {
+    var access = document.getElementById("presentacion");
+
+
+    access.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrolltoId2() {
+    var access = document.getElementById("formacion");
+
+
+    access.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrolltoId3() {
+    var access = document.getElementById("experiencia");
+
+
+    access.scrollIntoView({ behavior: 'smooth' }, true);
+}
